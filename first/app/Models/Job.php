1b472e65647ad6d1ce073c35b3dca627bac0810e
Filipe-Silva-01 <1231756@isep.ipp.pt>
2025-03-12 00:37:21 +0000

    Laracasts - Day 8 - Migrations - Done

--- first/app/Models/Job.php
+++ first/app/Models/Job.php
@@ -2,40 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job  extends Model
 {
-    public static function all():array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Engineer',
-                'salary' => '10,000€',
-            ],
-            [
-                'id'=> 2,
-                'title' => 'Programmer',
-                'salary' => '2,500€',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '5,000€'
-            ]
-        ];
-    }
-
-    public static function find(int $id):array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-        if (!$job)
-        {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
